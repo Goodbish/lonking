4249eca6c2e25f8d8204094ced397995e51609b3
Daniil <goodbish@mail.ru>
2022-04-20 20:46:07 +0500

    animations + select

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -414,6 +414,7 @@ modal();
 const select1 = new CustomSelect('#select-1');
 const select2 = new CustomSelect('#select-2');
 
+
 const activeSelects = document.querySelectorAll('.header .header__location-active , .header--mobile .header__location-active');
 const menuCity = document.querySelector('.menu .header__location-active');
 
@@ -425,12 +426,24 @@ const menuCity = document.querySelector('.menu .header__location-active');
 // })
 
 document.querySelector('#select-2').addEventListener('select.change', (e) => {
-
     const selected = e.target.querySelector('.select__option_selected');
     const text = selected ? selected.textContent : '';
     menuCity.innerHTML = text;
     console.log(`Выбранный текст опции: ${text}`);
 });
+
+const contactsSelect = document.querySelector('#select-3');
+
+if (contactsSelect !== null) {
+    const select3 = new CustomSelect('#select-3');
+    contactsSelect.addEventListener('select.change', (e) => {
+        const btn = e.target.querySelector('.select__toggle');
+        const selectedValue = btn.value;
+        select1.value = selectedValue;
+        select2.value = selectedValue;
+    })
+}
+
 let galleryThumbs = new Swiper(".product__bottom", {
     spaceBetween: 24,
     slidesPerView: 3,
